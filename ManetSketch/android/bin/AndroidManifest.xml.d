34a0fdcec8fe630e7f23553c41c2f760fff74947
jrobble <jrobble@mitre.org>
2013-01-17 15:46:25 -0500

    Now ignoring exported android directory.

--- ManetSketch/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/dev/git/android-manet-visualizer/ManetSketch/android/bin/AndroidManifest.xml : \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/AndroidManifest.xml \
--- ManetSketch/android/bin/ManetSketch-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/home/dev/git/android-manet-visualizer/ManetSketch/android/bin/ManetSketch-debug-unaligned.apk : \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/bin/ManetSketch.ap_ \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/bin/classes.dex \
-/home/dev/Desktop/android-sdk-linux_x86/tools/support/annotations.jar \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/libs/apwidgets.jar \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/libs/processing-core.jar \
-/home/dev/git/android-manet-visualizer/ManetSketch/android/libs/libmanet.jar \
